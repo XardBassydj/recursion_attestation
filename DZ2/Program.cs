eb984071713da7d1d4959b1f61b4a5738eda6cdb
unknown <evmel28@gmail.com>
2024-02-12 19:46:25 +0300

    second tasc

---
+++ DZ2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Пример использования
+        int m = 2;
+        int n = 1;
+        Console.WriteLine($"Значение функции Аккермана для m={m} и n={n}: {Ackermann(m, n)}");
+    }
+}
