ca810576c9c713dd0e55e9cb56021e5309fe8f6e
unknown <evmel28@gmail.com>
2024-02-12 19:47:07 +0300

    third tasc

---
+++ DZ3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 2, 5, 0, 10, 34 };
+int size = array.Length;
+
+void ShowArray(int[] arr, int size)
+    {
+        if (size == 0)
+        {
+            Console.Write(arr[size]);
+            return;
+        }
+        Console.Write(arr[size - 1] + " ");
+        ShowArray(arr, size - 1);
+    }
+Console.WriteLine("Элементы массива:");
+ShowArray(array, size);    
