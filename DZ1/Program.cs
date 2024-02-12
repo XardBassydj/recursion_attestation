18d41c88905f86c8c1c3606e9c1f06f5c0dc337b
unknown <evmel28@gmail.com>
2024-02-12 19:45:52 +0300

    first tasc

---
+++ DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿int m = 1;
+int n = 10;
+     void ShowNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            ShowNumbers(m + 1, n);
+        }
+    }
+    
+ Console.WriteLine($"Натуральные числа в промежутке от {m} до {n}:");
+ ShowNumbers(m, n);
+    
